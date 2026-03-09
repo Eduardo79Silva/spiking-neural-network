b4ab9034de1f0a51f7ef18e3a5106d3fc67d578d
Eduardo Silva <eduardo4silva@gmail.com>
2026-03-09 14:52:21 +0100

    feat: create synapse intermediary to calculate the current passed onto the post layer

---
+++ synapse.py
@@ -0,0 +1,20 @@
+import numpy as np
+from layers.spiking_layer import SpikingLayer
+
+
+class Synapse:
+
+    def __init__(
+        self, pre_layer: SpikingLayer, post_layer: SpikingLayer, learning_rule=None
+    ):
+        self.pre_layer = pre_layer
+        self.post_layer = post_layer
+        self.weights = np.random.normal(
+            0.5, 0.1, (post_layer.num_neurons, pre_layer.num_neurons)
+        )
+        self.learning_rule = learning_rule
+
+    def step(self, t):
+        pre_spikes = self.pre_layer.last_spikes
+        post_spikes = self.post_layer.last_spikes
+        self.learning_rule.update(pre_spikes, post_spikes, self.weights, t)
