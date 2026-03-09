f31ae1bbbddd3f3163751279ee0bafddc2455ed3
Eduardo Silva <eduardo4silva@gmail.com>
2026-03-09 14:51:55 +0100

    refactor: change spiking layer to allow current to be calculated in synapse

--- layers/spiking_layer.py
+++ layers/spiking_layer.py
@@ -4,9 +4,8 @@ import numpy as np
 class SpikingLayer:
     def __init__(self, num_inputs: int, num_neurons: int, tau, v_rest, v_th, v_reset):
         self.num_neurons = num_neurons
-        self.num_inputs = num_inputs
 
-        self.weights = np.random.normal(0.5, 0.1, (num_neurons, num_inputs))
+        self.last_spikes = [0] * num_neurons
 
         self.v = np.full(num_neurons, v_rest)
         self.tau = tau
@@ -14,17 +13,15 @@ class SpikingLayer:
         self.v_th = v_th
         self.v_reset = v_reset
 
-    def step(self, input_spikes: np.ndarray, dt: float):
-        """
-        input_spikes: binary vector (1s and 0s) from the source/previous layer
-        """
-        current = np.dot(self.weights, input_spikes)
+    def step(self, input_current: np.ndarray, dt: float):
 
-        dv = (-(self.v - self.v_rest) / self.tau + current) * dt
+        dv = (-(self.v - self.v_rest) / self.tau + input_current) * dt
         self.v += dv
 
         spikes = (self.v >= self.v_th).astype(float)
 
         self.v[spikes > 0] = self.v_reset
+
+        self.last_spikes = spikes
 
         return spikes
