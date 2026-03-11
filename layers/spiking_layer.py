37c7a26e2a93596990e84f1655eab7af873ca6e4
Eduardo Silva <eduardo4silva@gmail.com>
2026-03-11 11:25:22 +0100

    refactor(layer): decouple input_current from step() signature

--- layers/spiking_layer.py
+++ layers/spiking_layer.py
@@ -5,6 +5,8 @@ class SpikingLayer:
     def __init__(self, num_inputs: int, num_neurons: int, tau, v_rest, v_th, v_reset):
         self.num_neurons = num_neurons
 
+        self.input_current = np.ndarray(shape=(1,))
+
         self.last_spikes = [0] * num_neurons
 
         self.v = np.full(num_neurons, v_rest)
@@ -13,9 +15,9 @@ class SpikingLayer:
         self.v_th = v_th
         self.v_reset = v_reset
 
-    def step(self, input_current: np.ndarray, dt: float):
+    def step(self, dt: float):
 
-        dv = (-(self.v - self.v_rest) / self.tau + input_current) * dt
+        dv = (-(self.v - self.v_rest) / self.tau + self.input_current) * dt
         self.v += dv
 
         spikes = (self.v >= self.v_th).astype(float)
@@ -23,5 +25,8 @@ class SpikingLayer:
         self.v[spikes > 0] = self.v_reset
 
         self.last_spikes = spikes
+
+        print(self.input_current)
+        print()
 
         return spikes
