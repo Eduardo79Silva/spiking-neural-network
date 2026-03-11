a7e77607981211436e8542472feda644e3b7579b
Eduardo Silva <eduardo4silva@gmail.com>
2026-03-11 11:25:23 +0100

    feat(network): add run() orchestrator and optional learning rule to create_synapse()

--- network.py
+++ network.py
@@ -1,17 +1,39 @@
+import numpy as np
+
 from layers.spiking_layer import SpikingLayer
 from synapse import Synapse
 
 
 class Network:
 
-    def __init__(self, layers: SpikingLayer = []):
-        self.layers = []
-        self.synapses = []
+    def __init__(self, layers: list[SpikingLayer] = [], timesteps: int = 50):
+        self.layers = layers
+        self.timesteps = timesteps
+        self.synapses: list[Synapse] = []
+        self.spikes = []
 
     def add_layer(self, layer: SpikingLayer):
         self.layers.append(layer)
 
-    def create_synapse(self, pre_layer_id: int, post_layer_id: int):
+    def create_synapse(self, pre_layer_id: int, post_layer_id: int, learning_rule=None):
         self.synapses.append(
-            Synapse(self.layers[pre_layer_id], self.layers[post_layer_id])
+            Synapse(
+                self.layers[pre_layer_id], self.layers[post_layer_id], learning_rule
+            )
         )
+
+    def run(self):
+        for t in range(self.timesteps):
+            for synapse in self.synapses:
+                synapse.compute_current()
+
+            for layer in self.layers:
+                layer.step(t)
+
+            for synapse in self.synapses:
+                synapse.update_weights(t)
+
+            self.spikes.append(self.layers[-1].last_spikes)
+
+    def get_output_spikes(self):
+        return np.array(self.spikes).T
